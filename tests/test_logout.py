27407e9e8c3723a9a6fda43df1da789a0ed01549
Oksana-Fedorowa <fyodorowa.oksana2011@yandex.ru>
2024-07-15 19:22:45 +0300

    new fixture

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -2,16 +2,12 @@ import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from locators import TestLocators
-from conftest import driver, login
 from constants import BASE_URL
+from conftest import logged_in_driver, driver, login
 
-def test_logout_from_personal_account(driver, login):
+def test_logout_from_personal_account(logged_in_driver):
+    driver = logged_in_driver
     driver.get(BASE_URL)
-
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(TestLocators.button_login_in_main)).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(TestLocators.input_email_auth)).send_keys(login.email)
-    driver.find_element(*TestLocators.input_password_auth).send_keys(login.password)
-    driver.find_element(*TestLocators.button_login).click()
 
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(TestLocators.button_personal_account)).click()
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(TestLocators.profile))
